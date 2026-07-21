2dddcc21b0b3cac851fe79ef4a68d69b1b556401
AppStudioPro <info@webdesignpros365.com>
2026-07-21 09:21:36 +0000

    fix: add bottom padding to blog grid so it doesn't sit on footer

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -76,7 +76,7 @@ export default function BlogPage() {
         </Link>
 
         {/* Card grid */}
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 pb-16">
           {rest.map((post) => (
             <Link
               key={post.slug}
